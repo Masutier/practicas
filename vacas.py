5e6eb670d6953e8d70953d1c12178203540b2e37
Masutier <masutier@gmail.com>
2023-03-05 14:25:40 -0500

    Code vacas.py with connection to mongo db

--- vacas.py
+++ vacas.py
@@ -25,10 +25,63 @@ def milkingCows():
 
 
 def listAll():
-
+    milking = []
     for x in vacasColl.find():
-        print(x)
+        milking.append(x)
+
+    day1 = int(milking[0]["0"][0]) + int(milking[0]["0"][1])
+    day2 = int(milking[0]["1"][0]) + int(milking[0]["1"][1])
+    tot1 = day1 + day2
+
+    print("week 1:", milking[0])
+    print("week 1 day 1:", milking[0]["0"])
+    print("week 1 day 2:", milking[0]["1"])
+    print("Total production week 1 day 1:", day1, " Liters")
+    print("Total production week 1 day 2:", day2, " Liters")
+    print("*" * 40)
+    print("Total production week 1:", tot1, " Liters")
+
+
+def updateReg(docId, dayUp, cow1Up, cow2Up):
+    from bson.objectid import ObjectId
+    _id = ObjectId(docId)
+
+    updates = {
+        "$set": {dayUp: [cow1Up, cow2Up]}
+    }
+
+    vacasColl.update_one({"_id": _id}, updates)
+
+
+def deleteReg(docId):
+    from bson.objectid import ObjectId
+    _id = ObjectId(docId)
+
+    vacasColl.delete_one({"_id": _id})
 
 
 
+#("******************************************")
+
+for x in vacasColl.find():
+    print(x)
+
+# CREATE DOCUMENT
+#milkingCows()
+
+# LIST ALL
 listAll()
+
+# UPDATE
+#docId = input("Id of Week to Update: ")
+#dayUp = input("Day to Update (0 ~ n): ")
+#cow1Up = input("Cow 1 Production: ")
+#cow2Up = input("Cow 2 Production: ")
+#updateReg(docId, dayUp, cow1Up, cow2Up)
+
+# DELETE
+#docId = input("Delete week: ")
+#deleteReg(docId)
+
+for x in vacasColl.find():
+    print(x)
