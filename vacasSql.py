874cce0f1eb80a7a76251ccc46b6940c3e67dfe0
Masutier <masutier@gmail.com>
2023-03-05 15:54:33 -0500

    Rename vacas.py for vacasMongo.py and Code vacasSql.py with connection to Sql db

---
+++ vacasSql.py
@@ -0,0 +1,69 @@
+import sqlite3 as sql
+
+conn=sql.connect("/home/gabriel/prog/matrix/cows.db")
+cursor=conn.cursor()
+
+
+# Crea la db
+def createDB():
+    conn.commit()
+    conn.close()
+
+
+# Crea la tabla en la db
+def createTable():
+    cursor.execute(
+        """CREATE TABLE milk (
+            day INTEGER,
+            vaca INTEGER,
+            liters INTEGER
+        )"""
+    )
+    conn.commit()
+    conn.close()
+
+
+# crea una fila en la db
+def insertRowData(day, vaca, liters):
+    sqlQuery = f"""INSERT INTO milk VALUES (
+        {day},
+        '{vaca}',
+        '{liters}'
+    )"""
+    cursor.execute(sqlQuery)
+    conn.commit()
+    conn.close()
+
+
+# Leer db y ordenar data
+# fields === day, vaca, liters
+def readOrdered(field):
+    sqlQuery = f"""SELECT * FROM milk ORDER BY {field}"""
+    cursor.execute(sqlQuery)
+    datos = cursor.fetchall()
+    conn.commit()
+    conn.close()
+    for dato in datos:
+        print(dato)
+
+
+
+# CREATE DATABASE
+#createDB()
+# CREATE TABLE
+#createTable()
+
+# CREATE REGISTER
+"""
+day = input("Day to Update (0 ~ n): ")
+vaca = input("Cow: ")
+liters = input("Cow Production: ")
+
+insertRowData(day, vaca, liters)
+"""
+
+# GET DATA ORDER BY FIELD
+
+field = input("Enter field to order: ")
+readOrdered(field)
+
